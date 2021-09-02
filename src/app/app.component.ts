26cbfe8b067cb5bcc0598659f800b0ee346be1f9
Joseph Szumowski <jszumowskijr@gmail.com>
2021-09-02 17:07:14 -0400

    added ngFor to replace hardcoded table, fixed isSpaceDebris

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,10 @@ export class AppComponent {
 		searchTerm = searchTerm.toLowerCase();
 		for(let i=0; i < this.sourceList.length; i++) {
 			let name = this.sourceList[i].name.toLowerCase();
+			let type = this.sourceList[i].type.toLowerCase();
 			if (name.indexOf(searchTerm) >= 0) {
+				matchingSatellites.push(this.sourceList[i]);
+			} else if (type.indexOf(searchTerm) >= 0) {
 				matchingSatellites.push(this.sourceList[i]);
 			}
 		}
@@ -50,6 +53,4 @@ export class AppComponent {
 		// this will cause Angular to re-make the table, but now only containing matches
 		this.displayList = matchingSatellites;
 	}
-
-
 }
